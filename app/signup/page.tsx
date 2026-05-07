ad46f9a63a8b748b60aaefeb0ca5a379736f3b65
dhairya2009 <dhairyaagrawal199@gmail.com>
2026-05-07 20:36:30 +0530

    Files Changes

---
+++ app/signup/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+function page() {
+  return (
+    <div>
+      <Header />
+      SignUp
+      <Footer />
+    </div>
+  );
+}
+
+export default page;
